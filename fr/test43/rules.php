97f7a1e018e01bdb4b03d716cc329c6bdac96cee
Typolib <al2c-typolib@googlegroups.com>
2015-05-26 23:37:03 +0200

    Adding new rule in /fr/test43

--- fr/test43/rules.php
+++ fr/test43/rules.php
@@ -1,1 +1,1 @@
-a:3:{s:4:"name";s:6:"test43";s:6:"common";b:0;s:5:"rules";a:0:{}}
+a:3:{s:4:"name";s:6:"test43";s:6:"common";b:0;s:5:"rules";a:1:{i:0;a:3:{s:7:"content";a:2:{i:0;s:1:"'";i:1;s:3:"’";}s:4:"type";s:7:"if_then";s:7:"comment";s:21:"Must use curly quote.";}}}
