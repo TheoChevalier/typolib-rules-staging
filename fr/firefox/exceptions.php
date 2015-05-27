1afef67ec78f76807c8f58de498bd06b61182bcd
Typolib <al2c-typolib@googlegroups.com>
2015-05-27 18:16:05 +0200

    Adding new exception in /fr/firefox

--- fr/firefox/exceptions.php
+++ fr/firefox/exceptions.php
@@ -1,1 +1,1 @@
-a:1:{s:10:"exceptions";a:1:{i:0;a:2:{s:7:"rule_id";s:1:"1";s:7:"content";s:7:"[<...>]";}}}
+a:1:{s:10:"exceptions";a:2:{i:0;a:2:{s:7:"rule_id";s:1:"1";s:7:"content";s:7:"[<...>]";}i:1;a:2:{s:7:"rule_id";s:1:"1";s:7:"content";s:15:"...\n...\n...\n";}}}
