42f9227130f7921c48fe4ca4c81087eb0e1b8d28
Typolib <al2c-typolib@googlegroups.com>
2015-05-27 15:47:00 +0200

    Adding new rule in /fr/Test3

--- fr/Test3/rules.php
+++ fr/Test3/rules.php
@@ -1,1 +1,1 @@
-a:2:{s:4:"name";s:5:"Test1";s:6:"common";b:0;}
+a:3:{s:4:"name";s:5:"Test1";s:6:"common";b:0;s:5:"rules";a:1:{i:0;a:2:{s:7:"content";a:2:{i:0;s:2:"«";i:1;s:2:"»";}s:4:"type";s:14:"quotation_mark";}}}
