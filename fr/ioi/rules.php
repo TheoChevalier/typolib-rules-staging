564ddca13e7066f072c0aa98853160efb8cb3bfd
Typolib <al2c-typolib@googlegroups.com>
2015-05-27 14:10:53 +0200

    Adding new rule in /fr/ioi

--- fr/ioi/rules.php
+++ fr/ioi/rules.php
@@ -1,1 +1,1 @@
-a:2:{s:4:"name";s:3:"ioi";s:6:"common";b:0;}
+a:3:{s:4:"name";s:3:"ioi";s:6:"common";b:0;s:5:"rules";a:1:{i:0;a:3:{s:7:"content";a:2:{i:0;s:1:"x";i:1;s:1:"y";}s:4:"type";s:7:"if_then";s:7:"comment";s:2:"cc";}}}
