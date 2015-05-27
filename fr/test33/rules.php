aa3afc6f09abcf4e62c3132fac3abfba5311dd7a
Typolib <al2c-typolib@googlegroups.com>
2015-05-27 15:55:40 +0200

    Deleting "test33" code.

--- fr/test33/rules.php
+++
@@ -1,1 +0,0 @@
-a:3:{s:4:"name";s:10:"test33-bis";s:6:"common";b:0;s:5:"rules";a:4:{i:0;a:3:{s:7:"content";s:19:"IF […] THEN […]";s:4:"type";s:7:"if_then";s:7:"comment";s:4:"test";}i:1;a:3:{s:7:"content";s:16:"This is my rule.";s:4:"type";s:7:"if_then";s:7:"comment";s:3:"Yop";}i:2;a:2:{s:7:"content";s:3:"yop";s:4:"type";s:7:"if_then";}i:3;a:2:{s:7:"content";s:3:"yop";s:4:"type";s:7:"if_then";}}}
