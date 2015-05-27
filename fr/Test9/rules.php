2b73d0ef731335f2408db0d8c464135fa68b2dd5
Typolib <al2c-typolib@googlegroups.com>
2015-05-27 13:59:11 +0200

    Editing rule in /fr/Test9

--- fr/Test9/rules.php
+++ fr/Test9/rules.php
@@ -1,1 +1,1 @@
-a:3:{s:4:"name";s:5:"Test9";s:6:"common";b:0;s:5:"rules";a:1:{i:1;a:3:{s:7:"content";a:2:{i:0;s:2:"ff";i:1;s:2:"yy";}s:4:"type";s:7:"if_then";s:7:"comment";s:3:"kjk";}}}
+a:3:{s:4:"name";s:5:"Test9";s:6:"common";b:0;s:5:"rules";a:0:{}}
