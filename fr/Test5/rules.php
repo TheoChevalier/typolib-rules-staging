3a84aa27da72d0859186a37e9d712a4b4d490202
Typolib <al2c-typolib@googlegroups.com>
2015-06-03 04:16:50 +0200

    Adding new rule in /fr/Test5

--- fr/Test5/rules.php
+++ fr/Test5/rules.php
@@ -1,1 +1,1 @@
-a:3:{s:4:"name";s:6:"Test69";s:6:"common";b:0;s:5:"rules";a:0:{}}
+a:3:{s:4:"name";s:6:"Test69";s:6:"common";b:0;s:5:"rules";a:1:{i:0;a:3:{s:7:"content";a:2:{i:0;s:4:"plop";i:1;s:4:"plop";}s:4:"type";s:12:"replace_with";s:7:"comment";s:14:"fdfdfdfdfdfdfd";}}}
