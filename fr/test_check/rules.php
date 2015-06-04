a4cccb51351202c2b061681efae54d0c32ba3835
Typolib <al2c-typolib@googlegroups.com>
2015-06-04 19:45:53 +0200

    Editing rule in /fr/test_check

--- fr/test_check/rules.php
+++ fr/test_check/rules.php
@@ -1,1 +1,1 @@
-a:3:{s:4:"name";s:10:"test check";s:6:"common";b:0;s:5:"rules";a:2:{i:0;a:2:{s:7:"content";a:2:{i:0;s:3:"...";i:1;s:3:"…";}s:4:"type";s:12:"replace_with";}i:3;a:3:{s:7:"content";a:2:{i:0;s:1:" ";i:1;s:2:"»";}s:4:"type";s:12:"check_before";s:7:"comment";s:15:"Espace avant »";}}}
+a:3:{s:4:"name";s:10:"test check";s:6:"common";b:0;s:5:"rules";a:1:{i:0;a:2:{s:7:"content";a:2:{i:0;s:3:"...";i:1;s:3:"…";}s:4:"type";s:12:"replace_with";}}}
