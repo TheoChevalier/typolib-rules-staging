684b27ac7950079c0510361b2ad0803d4a72f35d
Typolib <al2c-typolib@googlegroups.com>
2015-06-08 16:05:32 +0200

    Revert "Deleting "" code."
    This reverts commit 880d4a4ee0a93f76f2ae68cd9b5a1a4cd0c18b0e.

--- fr/aaaaaa/rules.php
+++ fr/aaaaaa/rules.php
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 a:3:{s:4:"name";s:6:"aaaaaa";s:6:"common";b:0;s:5:"rules";a:4:{i:0;a:3:{s:7:"content";a:2:{i:0;s:1:"i";i:1;s:1:"k";}s:4:"type";s:12:"replace_with";s:7:"comment";s:31:"This is a test for a comment. 2";}i:1;a:2:{s:7:"content";a:2:{i:0;s:2:"pl";i:1;s:2:"pl";}s:4:"type";s:12:"replace_with";}i:2;a:2:{s:7:"content";a:2:{i:0;s:3:"pl2";i:1;s:3:"pl2";}s:4:"type";s:12:"replace_with";}i:3;a:2:{s:7:"content";a:2:{i:0;s:3:"pl3";i:1;s:3:"pl3";}s:4:"type";s:12:"replace_with";}}}
+=======
+a:3:{s:4:"name";s:6:"aaaaaa";s:6:"common";b:0;s:5:"rules";a:2:{i:0;a:3:{s:7:"content";a:2:{i:0;s:1:"i";i:1;s:1:"i";}s:4:"type";s:12:"replace_with";s:7:"comment";s:31:"This is a test for a comment. 2";}i:1;a:3:{s:7:"content";a:2:{i:0;s:1:"u";i:1;s:1:"u";}s:4:"type";s:14:"quotation_mark";s:7:"comment";s:3:"kjh";}}}
+>>>>>>> parent of 880d4a4... Deleting "" code.
