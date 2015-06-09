5ccde8b401b3a0c8f0fb2d73126fb0ff9d8a0ba9
Typolib <al2c-typolib@googlegroups.com>
2015-06-09 12:04:34 +0200

    Adding new exception in /fr/test_check

--- fr/test_check/exceptions.php
+++ fr/test_check/exceptions.php
@@ -1,1 +1,1 @@
-a:1:{s:10:"exceptions";a:3:{i:1;a:2:{s:7:"rule_id";s:1:"5";s:7:"content";s:3:"»,";}i:2;a:2:{s:7:"rule_id";s:2:"11";s:7:"content";s:3:"%;t";}i:3;a:2:{s:7:"rule_id";s:1:"1";s:7:"content";s:5:""var"";}}}
+a:1:{s:10:"exceptions";a:4:{i:1;a:2:{s:7:"rule_id";s:1:"5";s:7:"content";s:3:"»,";}i:2;a:2:{s:7:"rule_id";s:2:"11";s:7:"content";s:3:"%;t";}i:3;a:2:{s:7:"rule_id";s:1:"1";s:7:"content";s:5:""var"";}i:4;a:2:{s:7:"rule_id";i:13;s:7:"content";N;}}}
