183b79a519d3113d0315249a2083dcc3ab0bd836
Typolib <al2c-typolib@googlegroups.com>
2015-06-16 17:42:59 +0200

    Adding new rule in /ro/test

--- ro/test/rules.php
+++ ro/test/rules.php
@@ -1,1 +1,1 @@
-a:3:{s:4:"name";s:4:"test";s:6:"common";b:1;s:5:"rules";a:1:{i:0;a:2:{s:7:"content";a:1:{i:0;s:1:";";}s:4:"type";s:16:"plural_separator";}}}
+a:3:{s:4:"name";s:4:"test";s:6:"common";b:1;s:5:"rules";a:2:{i:0;a:2:{s:7:"content";a:1:{i:0;s:1:";";}s:4:"type";s:16:"plural_separator";}i:1;a:2:{s:7:"content";a:2:{i:0;s:1:" ";i:1;s:1:";";}s:4:"type";s:11:"check_after";}}}
