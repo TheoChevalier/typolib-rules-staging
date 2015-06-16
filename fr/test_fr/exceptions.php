407bcc8927777f0402f4f4fe882f57c1f9713e1c
Typolib <al2c-typolib@googlegroups.com>
2015-06-16 23:11:23 +0200

    Editing exception in /fr/test_fr

--- fr/test_fr/exceptions.php
+++ fr/test_fr/exceptions.php
@@ -1,1 +1,1 @@
-a:1:{s:10:"exceptions";a:1:{i:0;a:2:{s:7:"rule_id";s:1:"0";s:7:"content";s:7:"u...u 5";}}}
+a:1:{s:10:"exceptions";a:1:{i:0;a:2:{s:7:"rule_id";s:1:"0";s:7:"content";s:7:"u...u 6";}}}
