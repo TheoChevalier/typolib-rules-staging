26d0841f85644b7f1a2d3a82b2c29990265ecc8d
Typolib <al2c-typolib@googlegroups.com>
2015-06-17 15:04:40 +0200

    Editing rule 1 in /ro/common

--- ro/common/rules.php
+++ ro/common/rules.php
@@ -1,1 +1,1 @@
-a:2:{s:4:"name";s:6:"common";s:5:"rules";a:2:{i:0;a:2:{s:7:"content";a:2:{i:0;s:1:"T";i:1;s:1:"H";}s:4:"type";s:12:"replace_with";}i:1;a:2:{s:7:"content";a:2:{i:0;s:3:" ";i:1;s:1:"%";}s:4:"type";s:12:"check_before";}}}
+a:2:{s:4:"name";s:6:"common";s:5:"rules";a:1:{i:0;a:2:{s:7:"content";a:2:{i:0;s:1:"T";i:1;s:1:"H";}s:4:"type";s:12:"replace_with";}}}
