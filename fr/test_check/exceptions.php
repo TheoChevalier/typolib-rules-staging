fd1d9b08ffa82e1ef644ce094876f60caf9b5668
Typolib <al2c-typolib@googlegroups.com>
2015-06-17 18:15:42 +0200

    Adding exception 5 in /fr/test_check

--- fr/test_check/exceptions.php
+++ fr/test_check/exceptions.php
@@ -1,1 +1,1 @@
-a:1:{s:10:"exceptions";a:3:{i:2;a:2:{s:7:"rule_id";s:2:"11";s:7:"content";s:3:"%;t";}i:3;a:2:{s:7:"rule_id";s:1:"1";s:7:"content";s:5:""var"";}i:4;a:2:{s:7:"rule_id";i:13;s:7:"content";s:5:"u...u";}}}
+a:1:{s:10:"exceptions";a:4:{i:2;a:2:{s:7:"rule_id";s:2:"11";s:7:"content";s:3:"%;t";}i:3;a:2:{s:7:"rule_id";s:1:"1";s:7:"content";s:5:""var"";}i:4;a:2:{s:7:"rule_id";i:13;s:7:"content";s:5:"u...u";}i:5;a:2:{s:7:"rule_id";s:2:"20";s:7:"content";s:3:"p.s";}}}
