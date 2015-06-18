fca891ce4fb7d2f8203780d4a53d29ff1c4ae0f9
Typolib <al2c-typolib@googlegroups.com>
2015-06-18 16:06:00 +0200

    Adding rule 1 in /en/Apostrophe

--- en/Apostrophe/rules.php
+++ en/Apostrophe/rules.php
@@ -1,1 +1,1 @@
-a:3:{s:4:"name";s:10:"Apostrophe";s:6:"common";b:0;s:5:"rules";a:1:{i:0;a:2:{s:7:"content";a:2:{i:0;s:3:"∅";i:1;s:1:"'";}s:4:"type";s:12:"check_before";}}}
+a:3:{s:4:"name";s:10:"Apostrophe";s:6:"common";b:0;s:5:"rules";a:2:{i:0;a:2:{s:7:"content";a:2:{i:0;s:3:"∅";i:1;s:1:"'";}s:4:"type";s:12:"check_before";}i:1;a:2:{s:7:"content";a:2:{i:0;s:3:"∅";i:1;s:1:"'";}s:4:"type";s:11:"check_after";}}}
