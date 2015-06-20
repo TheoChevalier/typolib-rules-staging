95e940809af53547ca708b82e60c95e6f198ef31
Typolib <al2c-typolib@googlegroups.com>
2015-06-20 04:03:58 +0200

    Adding rule 0 in /fr/test

--- fr/test/rules.php
+++ fr/test/rules.php
@@ -1,1 +1,1 @@
-a:2:{s:4:"name";s:4:"test";s:6:"common";b:0;}
+a:3:{s:4:"name";s:4:"test";s:6:"common";b:0;s:5:"rules";a:1:{i:0;a:2:{s:7:"content";a:2:{i:0;s:4:"plop";i:1;s:4:"plop";}s:4:"type";s:12:"replace_with";}}}
